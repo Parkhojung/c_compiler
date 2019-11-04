409d3f5c107c9f57bd1a0e98bd744d75925089d1
Hojung Park <parkhj062@gmail.com>
2019-11-04 17:22:13 +0900

    scan.l function implemented

--- c_syntax_analysis/c_syntax_analysis/input.c
+++ c_syntax_analysis/c_syntax_analysis/input.c
@@ -1,21 +1,3 @@
-//
-//  input.c
-//  c_syntax_analysis
-//
-//  Created by PARKHOJUNG on 2019/11/03.
-//  Copyright © 2019 PARKHOJUNG. All rights reserved.
-//
-
-int main(){
-    
-    int a;
-    a = 3;
-    
-    int b;
-    b = 6;
-    
-    int c = a+b;
-    
-    
-    return 0;
-}
+int a = 3;
+int b = 5;
+int c = 10;
