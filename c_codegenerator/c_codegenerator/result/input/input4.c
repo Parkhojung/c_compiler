086f13caad17b65b246e7552f0dcb3c1a33ba62e
i386-ubuntu-phj <parkhj062@gmail.com>
2019-12-17 02:42:35 +0900

    while i386 interpretor debugging

--- c_codegenerator/c_codegenerator/result/input/input4.c
+++ c_codegenerator/c_codegenerator/result/input/input4.c
@@ -1,4 +1,4 @@
-	      INT   0, 20
+	      INT   0, 28
 	      SUP   0, main
 	      RET   0, 0
 main:
@@ -35,8 +35,44 @@ main:
 	     LITI   0, 20
 	      STX   0, 1
 	      POP   0, 1
+	      INT   0, 12
+	      LDA   0, 20
+	      LDA   1, 12
+	     LITI   0, 0
+	     LITI   0, 8
+	     MULI   0, 0
+	   OFFSET   0, 0
+	      LDI   0, 1
+	      LDA   1, 12
+	     LITI   0, 0
+	     LITI   0, 80
+	     MULI   0, 0
+	   OFFSET   0, 0
+	      LDI   0, 2
+	      LDI   0, 1
+	      LDA   1, 12
+	     LITI   0, 0
+	     LITI   0, 8
+	     MULI   0, 0
+	   OFFSET   0, 0
+	     LITI   0, 4
+	   OFFSET   0, 0
+	      LDI   0, 1
+	      LDA   1, 12
+	     LITI   0, 0
+	     LITI   0, 80
+	     MULI   0, 0
+	   OFFSET   0, 0
+	      LDI   0, 2
+	     LITI   0, 4
+	   OFFSET   0, 0
+	      LDI   0, 1
+	      POP   0, 6
+	     ADDR   0, printf
+	      CAL   0, 0
 	      LDA   1, -4
 	     LITI   0, 0
 	      STO   0, 1
 	      RET   0, 0
 	      RET   0, 0
+.literal    20  "%d %d"
